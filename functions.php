81740bf78d64fcc034527fc7147b6fcd2fb5549d
James Bubb <jpbubb82@gmail.com>
2016-11-07 11:47:51 +0000

    Intialise repository (basic theme)

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/* Register styles */
+function register_styles(){
+  wp_register_style('main', get_stylesheet_directory_uri() . '/css/main.css');
+  wp_register_style('main_font','https://fonts.googleapis.com/css?family=Roboto');
+  wp_enqueue_style('main');
+  wp_enqueue_style('main_font');
+}
+
+add_action( 'wp_enqueue_scripts', 'register_styles' );
+?>
