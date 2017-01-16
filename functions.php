af59475092bf667364298bfef44a0621197d014f
James Bubb <jpbubb82@gmail.com>
2017-01-16 15:18:13 +0000

    Update to use prismjs

--- functions.php
+++ functions.php
@@ -3,16 +3,16 @@
 function register_styles(){
   wp_register_style('core', get_stylesheet_directory_uri() . '/style.css');
   wp_register_style('main', get_stylesheet_directory_uri() . '/css/main.css');
-  wp_register_style('highlightjs', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/styles/default.min.css');
+  wp_register_style('prismcss', get_stylesheet_directory_uri() . '/css/prism.css');
   wp_register_style('main_font','https://fonts.googleapis.com/css?family=Montserrat:400,700');
-  wp_register_script('highlightjsjs', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js');
+  wp_register_script('prismjs', get_stylesheet_directory_uri() . '/js/prism.js');
 
   wp_enqueue_style('core');
   wp_enqueue_style('main');
-  wp_enqueue_style('highlightjs');
+  wp_enqueue_style('prismcss');
   wp_enqueue_style('main_font');
 
-  wp_enqueue_script('highlightjsjs');
+  wp_enqueue_script('prismjs');
 }
 
 
--- header.php
+++ header.php
@@ -28,7 +28,7 @@
     <meta name="msapplication-TileColor" content="#f01d4f">
     <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
     <meta name="theme-color" content="#121212">
-
+    <link rel="alternate" href="https://juniordevelopercentral.com" hreflang="en-US" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
     <?php // wordpress head functions ?>
