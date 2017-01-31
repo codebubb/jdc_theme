b50add4ed5b334deed19e707be5365dd9f6fe9c2
James Bubb <jpbubb82@gmail.com>
2017-01-31 17:51:53 +0000

    Fix issue with homepage

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php get_header() ?>
 <div class="content">
 <div class="container">
-<?php
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <article class="list" id="post-<?php the_ID(); ?>" role="article">
