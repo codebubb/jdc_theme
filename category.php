bc5b950234a2178aedf47eb4f52ce4c025555360
James Bubb <jpbubb82@gmail.com>
2017-02-09 21:31:01 +0000

    Setup category pages

--- category.php
+++ category.php
@@ -1,13 +1,37 @@
 <?php get_header() ?>
 <div class="content">
 <div class="container">
-<h1>HowTos</h1>
 <?php
-  $howToCat = get_cat_ID('HowTo');
-  foreach(get_categories(array('child_of' => $howToCat)) as $howTo){
-    echo $howTo->name;
-  }
-?>
+$category = get_category(get_query_var('cat'));
+$args = array('category_name' => $category->name);
+$query = new WP_Query($args);
+
+if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+  <article class="list" id="post-<?php the_ID(); ?>" role="article">
+  <a href="<?php the_permalink() ?>">
+    <div class="featured-img">
+      <?php the_post_thumbnail('home-page-thumb'); ?>
+    </div>
+    <h2 class="article-title"><?php the_title(); ?></h2>  </a>
+    <p class="post-details"><?php echo the_modified_date(); ?></p>
+  </article>
+<?php endwhile; ?>
+
+<?php else : ?>
+
+  <article id="post-not-found" class="hentry cf">
+    <header class="article-header">
+      <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+    </header>
+    <section class="entry-content">
+      <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+    </section>
+    <footer class="article-footer">
+      <p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+    </footer>
+  </article>
 </div>
+<?php endif; ?>
+
 </div>
 <?php get_footer() ?>
--- page-howto.php
+++ page-howto.php
@@ -1,12 +1,19 @@
 <?php get_header() ?>
 <div class="content">
 <div class="container">
-<h1>HowTos</h1>
+<h1>How To Guides</h1>
 <?php
   $howToCat = get_cat_ID('HowTo');
-  foreach(get_categories(array('child_of' => $howToCat)) as $howTo){
-    echo $howTo->name;
-  }
+  foreach(get_categories(array('child_of' => $howToCat, 'hide_empty' => 0)) as $howTo){
+    ?><article class="category" role="article">
+    <a href="<?php echo get_category_link($howTo->term_id); ?>">
+      <div class="featured-img">
+        <?php if (function_exists('z_taxonomy_image')) z_taxonomy_image($howTo->term_id); ?>
+      </div>
+      <h2 class="category-title"><?php echo $howTo->name?></h2>
+    </a>
+    </article>
+  <?php }
 ?>
 </div>
 </div>
